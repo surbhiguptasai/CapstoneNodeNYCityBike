1629c33d4cb63c5e95eb3edfd2490252c7788b1f
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2021-03-22 23:11:24 -0400

    Commiting latest react

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://127.0.0.1/capstone-nycbike-app';
+                      'mongodb+srv://surbhi:surbhi@cluster0.p9ab3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = (
                           	process.env.TEST_DATABASE_URL ||
-	                   'mongodb://127.0.0.1/test-capstone-nycbike-app');
+	                   'mongodb+srv://surbhi:surbhi@cluster0.p9ab3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 exports.PORT = process.env.PORT || 8080;
